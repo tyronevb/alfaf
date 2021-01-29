09bda80fe1eb0df3cf4d853da271edfc38cd4e9e
Tyrone van Balla <tyronevb@gmail.com>
2021-01-30 01:55:46 +0200

    Apply consistent formatting and coding standards.
    Ran codebase through an auto-formatting and checked for compliance
    to coding standards. Made fixes as necessary.

--- scripts/run_lfa_framework.py
+++ scripts/run_lfa_framework.py
@@ -1,4 +1,4 @@
-"""Script to run the Cognitive Debugging, Automated LFA Framework"""
+"""Script to run the Cognitive Debugging, Automated LFA Framework."""
 
 __author__ = "tyronevb"
 __date__ = "2021"
@@ -7,21 +7,30 @@ import argparse
 import sys
 
 sys.path.append("..")
-from src.lfa_framework import AutomatedLFAFramework
+from src.lfa_framework import AutomatedLFAFramework  # noqa
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run the Automated LFA Framework in either Training or Inference Mode")
-    parser.add_argument("-d", "--data_miner_config", action="store", type=str,
-                        help="Path to configuration file (yaml) that contains "
-                             "the log format for the log files to be parsed, "
-                             "log parsing method and tunable parameter values",
-                        required=True)
-    parser.add_argument("-e", "--inference_engine_config", action="store",
-                        type=str,
-                        help="Path to configuration file (yaml) that contains "
-                             "the various parameters for the Inference Engine, "
-                             "and Feature Extractor",
-                        required=True)
+    parser.add_argument(
+        "-d",
+        "--data_miner_config",
+        action="store",
+        type=str,
+        help="Path to configuration file (yaml) that contains "
+        "the log format for the log files to be parsed, "
+        "log parsing method and tunable parameter values",
+        required=True,
+    )
+    parser.add_argument(
+        "-e",
+        "--inference_engine_config",
+        action="store",
+        type=str,
+        help="Path to configuration file (yaml) that contains "
+        "the various parameters for the Inference Engine, "
+        "and Feature Extractor",
+        required=True,
+    )
     parser.add_argument(
         "-i",
         "--input_dir",
@@ -46,21 +55,48 @@ if __name__ == "__main__":
         help="Name of raw log file to be provided to the Framework on. Should not include path.",
         required=True,
     )
-    parser.add_argument("-n", "--name", action="store", type=str,
-                        help="A unique name for this instance of the Framework. Used when naming output"
-                             "files from the framework. Reccommend using name representative of system under"
-                             "test",
-                        required=True)
-    parser.add_argument("-c", "--compute_device", action="store", choices=["cpu", "cuda:0", None], default=None,
-                        help="Specify which compute device to use for deep learning model training and inference")
-    parser.add_argument("-m", "--mode", action="store", choices=["training", "inference"], default="training",
-                        help="Specify which mode to operate the Framework in. Either training mode or inference mode. "
-                             "Configuration files must also be for the required mode.")
-    parser.add_argument("-s", "--datastore", action="store", type=str, default=None,
-                        help="Path to existing datastore containing log keys and events for the system in question. "
-                             "Is required when operating in Inference Mode")
-    parser.add_argument("-u", "--update_store", action="store_true",
-                        help="Specify whether the datastore should be updated if new log keys are found.")
+    parser.add_argument(
+        "-n",
+        "--name",
+        action="store",
+        type=str,
+        help="A unique name for this instance of the Framework. Used when naming output"
+        "files from the framework. Reccommend using name representative of system under"
+        "test",
+        required=True,
+    )
+    parser.add_argument(
+        "-c",
+        "--compute_device",
+        action="store",
+        choices=["cpu", "cuda:0", None],
+        default=None,
+        help="Specify which compute device to use for deep learning model training and inference",
+    )
+    parser.add_argument(
+        "-m",
+        "--mode",
+        action="store",
+        choices=["training", "inference"],
+        default="training",
+        help="Specify which mode to operate the Framework in. Either training mode or inference mode. "
+        "Configuration files must also be for the required mode.",
+    )
+    parser.add_argument(
+        "-s",
+        "--datastore",
+        action="store",
+        type=str,
+        default=None,
+        help="Path to existing datastore containing log keys and events for the system in question. "
+        "Is required when operating in Inference Mode",
+    )
+    parser.add_argument(
+        "-u",
+        "--update_store",
+        action="store_true",
+        help="Specify whether the datastore should be updated if new log keys are found.",
+    )
 
     args = parser.parse_args()
 
@@ -68,15 +104,17 @@ if __name__ == "__main__":
     print("Running Automated LFA Framework . . .\n")
 
     # create AutomatedLFAFramework instance
-    lfa_framework = AutomatedLFAFramework(data_miner_config=args.data_miner_config,
-                                          inference_engine_config=args.inference_engine_config,
-                                          input_dir=args.input_dir,
-                                          output_dir=args.output_dir,
-                                          name=args.name,
-                                          device=args.compute_device,
-                                          mode=args.mode,
-                                          datastore=args.datastore,
-                                          update_datastore=args.update_store)
+    lfa_framework = AutomatedLFAFramework(
+        data_miner_config=args.data_miner_config,
+        inference_engine_config=args.inference_engine_config,
+        input_dir=args.input_dir,
+        output_dir=args.output_dir,
+        name=args.name,
+        device=args.compute_device,
+        mode=args.mode,
+        datastore=args.datastore,
+        update_datastore=args.update_store,
+    )
 
     if args.mode == "training":
         # training mode
